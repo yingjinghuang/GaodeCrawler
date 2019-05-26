f362066568c57e3d5b1f638828e5f00bb9065821
RealIvyWong <924154233@qq.com>
2019-05-26 21:01:10 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # GaodeCrawler
-爬取高德poi，需要你自己的key，且注意key的限额
+
+项目链接：<https://github.com/RealIvyWong/GaodeCrawler>
+
+## 1 实现功能
+
+这个项目是爬取某个城市所有类别的高德 POI 数据，并写进 sqlite 数据库。
+
+会用到你自己的高德 API key。而且目前（2019 年 5 月）高德免费 API key 的配额是每天调用量为 2000（意思就是一个 key 只能访问 2000 个页面，一个页面可能 20+ 条数据），所以注意一天不要爬取太多。
+
+用这个代码爬取的武汉市的 POI 数据有 41w+ 条，还是比较多的了。可能一个中等城市爬个小半天左右吧。
+
+【注】 key 要是 **Web** 服务的。
+
+## 2 依赖环境
+
+使用的是 Python 3.7。
+
+无需额外的第三方库。
+
+## 3 使用方法
+
+**step1.** 修改 start.py 中的【自己设置区域】的三个变量 citycode（城市代码）, keypoolFile（key 池的文件），database（数据库的文件名，默认为 poi.sqlite)。
+
+**step2.** 修改 keypool.txt 文件中的 API key（每个 key 占一行）。
+
+**step2.** Run start.py。
+
+> 【**如何知道想要爬取的城市的citycode**】
+>
+> 打开当前文件夹中的 city.json 文件，搜索你所想爬取的文件名，对应的 citycode 就能看到。
+>
+> 比如说 {"city":"北京市","adcode":"110000","citycode":"010"} 北京市的 citycode，就是 010。
+
+## 4 文件说明
+
+包含三个文件。
+
+### start.py
+
+爬虫本体。
+
+### city.json
+
+城市列表数据。
+
+### amap_poicode.xlsx
+
+高德直接下载获得的 poi 列表。
+
+### amap_poicode.csv
+
+其实就是上面这个 xlsx 文件转为的 csv 文件，因为 csv 文件对于 python 程序更友好，所以它才有存在的必要。
+
+### keypool.txt
+
+key 池，每个 key 一行。
+
+## 5 爬取示例
+
+如果开始成功运行之后，控制台输出大概是这样的。代码的原理是先按整个城市范围进行搜索，然后再按各个区县进行搜索，这样可以获得更多的数据，所以一个城市的爬取会按一个个区域来爬。
+
+![1558873768062](https://github.com/RealIvyWong/ImageHosting/raw/master/assets/1558873768062.png)
+
+得到的`poi.sqlite`结构是包含一个 poi 表。
+
+表的字段有 id（序号）, poi_id（POI 自己的 id）, biz_type（好像是商业类型的数据，忘记了，一般都为空，不重要）, name（POI 名）, type（POI 类型，按分号划分大中小类三级）, address（地址）, tel（电话)，location（经纬度，目测是火星坐标系），pcode（省的代码），pname（省的名称），citycode（城市代码），cityname（城市名），adcode（区域代码），adnametext（区县名），business_area（所属商业区，不一定会有）。
+
+![1558875092443](https://github.com/RealIvyWong/ImageHosting/raw/master/assets/1558875092443.png)
+
+## 6 联系我
+
+如果有什么建议或意见，欢迎联系我（huangyingjing@whu.edu.cn)或者提 issue！
+
+## 7 请我喝杯咖啡
+
+如果我的代码帮助到了你，欢迎你请我喝杯咖啡~
+
+<img src="https://github.com/RealIvyWong/ImageHosting/raw/master/assets/支付宝收款码.jpg" height="30%" width="30%"> <img src="https://github.com/RealIvyWong/ImageHosting/raw/master/assets/微信收款码.png" height="30%" width="30%">
---
+++ start.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+# version: python 3.7
+# author: Ivy
+
+import json
+import requests
+import time,sys
+import sqlite3,random
+
+################### 自己设置区域 #####################
+citycode='022' # 需要爬取的城市代号
+keypoolFile='keypool.txt' # key池文件
+database='poi.sqlite' # 数据库的文件名
+####################################################
+
+def get_data(page_index, url_amap):
+    time.sleep(0.5)
+    print('解析页码: ' + str(page_index) + '... ...')
+    response = requests.get(url_amap)
+    poi_json = response.json()
+
+    if poi_json.get("info")=="DAILY_QUERY_OVER_LIMIT":
+        print('key超过限制了，换一个')
+        print(poi_json)
+        return "limit"
+
+    poi_lists = poi_json.get("pois")
+    if poi_lists != None or '':
+        if len(poi_json.get("pois"))<1:
+            return False
+        for poi in poi_lists:
+            poi_list = []
+            poi_list.append(poi.get('id'))
+            poi_list.append(poi.get('biz_type'))
+            poi_list.append(poi.get('name'))
+            poi_list.append(poi.get('type'))
+            poi_list.append(poi.get('address'))
+            poi_list.append(poi.get('tel'))
+            poi_list.append(poi.get('location'))
+            poi_list.append(poi.get('pcode'))
+            poi_list.append(poi.get('pname'))
+            poi_list.append(poi.get('citycode'))
+            poi_list.append(poi.get('cityname'))
+            poi_list.append(poi.get('adcode'))
+            poi_list.append(poi.get('adname'))
+            poi_list.append(poi.get('business_area'))
+            #print(poi_list)
+
+            try:
+                writeDb(poi_list)
+                #print('已保存到sqlite')
+            except Exception as e:
+                print('出错了！')
+                print(e)
+                print(poi_list)
+                sys.exit(1)
+
+    else:
+        pass
+    return poi_json.get("pois")
+
+def writeDb(poi_list):
+    try:
+        ins="INSERT or ignore INTO poi VALUES (null,"+",".join(["'%s'" %x for x in poi_list])+")"
+        cur.execute(ins)
+        conn.commit()
+        return True
+    except:
+        return False
+
+def initDb():
+    cre='''Create Table if not exists poi(Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    poi_id INTEGER UNIQUE,
+    biz_type TEXT,
+    name TEXT,
+    type TEXT,
+    address text,
+    tel text,
+    location text,
+    pcode text,
+    pname text,
+    citycode text,
+    cityname text,
+    adcode text,
+    adnametext,
+    business_area text)
+    '''
+    cur.execute(cre)
+    conn.commit()
+    print('数据库已准备好')
+
+def getKeypool(file):
+    with open(file, 'r', encoding='utf-8') as f:
+        rows=f.readlines()
+        keys=[]
+        for row in rows:
+            row=row.replace('\n','')
+            keys.append(row)
+        return keys
+
+def getPOIdata(poicode_now,city):
+    global key_pool
+    page=1
+    while True:
+        key=random.choice(key_pool)
+        url_amap = 'http://restapi.amap.com/v3/place/text?key={}&types={}&city={}&citylimit=true&children=1&offset=25&page={}&extensions=all'.format(key,poicode_now,city,page)
+        json_data = get_data(page, url_amap)
+
+        if json_data=="limit":
+            key_pool.remove(key)
+            if len(key_pool)<1:
+                print('key没存货啦！')
+                return False
+            continue
+
+        if not json_data:
+            print('当前类别爬完了')
+            break
+        page+=1
+    return True
+
+
+if __name__ == '__main__':
+
+    conn=sqlite3.connect(database)
+    cur=conn.cursor()
+    initDb()
+
+    # 加载城市列表
+    data = open("city.json", encoding="utf-8-sig")
+    strJson = json.load(data)
+
+    # 加载获取区域adcode的list
+    city = []
+    for i in range(len(strJson)): # 获取当前城市所有adcode的列表
+        if strJson[i]['citycode']==citycode:
+            city.append(strJson[i]['adcode'])
+
+    # 加载key池
+    global key_pool
+    key_pool=getKeypool(keypoolFile)
+
+    # 获取所有类别的代号
+    with open('amap_poicode.CSV','r',encoding='utf-8') as f:
+        rows=f.readlines()
+        poicode=[]
+        for row in rows:
+            row.replace('\n','')
+            temp_row=row.split(',')
+            if temp_row[1]=='NEW_TYPE' or temp_row[1]=='':
+                continue
+            poicode.append(temp_row[1])
+
+    # 开始一个个爬取
+    for y in range(len(city)):
+        print('开始第',y+1,'个区域 ','共有',len(city),'个区域')
+        for j in range(len(poicode)):
+            poicode_now=poicode[j]
+            print('爬到第',y+1,'个区域，第',j,'个类别了，代码为',poicode_now)
+            result=getPOIdata(poicode_now, city[y])
+            if not result: # key的额度不够了，就先退出程序
+                print('当前城市序号为',y,'，当前类别序号为',j)
+                cur.close()
+                conn.close()
+                sys.exit(1)
+
+    cur.close()
+    conn.close()
